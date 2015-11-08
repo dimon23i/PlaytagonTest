b80f0250511feb508eae3f7ecaf3a74edec8b2bd
TemetNosce <dimon23i@mail.ru>
2015-11-08 16:29:01 +0200

    add CRUD for characters

---
+++ PlaytagonTest.Web/App/Controllers/index.js
@@ -0,0 +1,41 @@
+﻿app.controller("IndexCtrl", function ($scope, $http, $uibModal) {
+    $scope.characters = [];
+
+    $scope.add = function () {
+        var model = {};
+
+        $scope.openModal(model)
+            .result.then(function (data) {
+                $scope.characters.push(data);
+            });
+    };
+    $scope.edit = function (item) {
+        var model = angular.copy(item);
+
+        $scope.openModal(model)
+            .result.then(function (data) {
+                angular.copy(data, item);
+            });
+    };
+    $scope.remove = function (array, index) {
+        array.remove(index);
+    };
+    $scope.openModal = function (model, templateId, controller) {
+        templateId = templateId || 'CharacterEditor';
+        controller = controller || 'ModalEdittCtrl';
+
+        return $uibModal.open({
+            animation: $scope.animationsEnabled,
+            templateUrl: templateId,
+            controller: controller,
+            size: "",
+            backdrop: 'static',
+            scope: $scope,
+            resolve: {
+                model: function () {
+                    return model;
+                }
+            }
+        });
+    };
+});
---
+++ PlaytagonTest.Web/App/Controllers/modal-edit.js
@@ -0,0 +1,10 @@
+﻿app.controller('ModalEditCtrl', ['$scope', '$modalInstance', 'model', function ($scope, $modalInstance, model) {
+    $scope.model = model;
+
+    $scope.ok = function () {
+        $modalInstance.close($scope.model);
+    };
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+}]);
